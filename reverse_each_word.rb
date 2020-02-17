63beef2c4384af615f105391659baa0c1d708afb
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-17 01:02:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   newarr = string.split
   newarr.collect do |i|
-    newstr = "#{i.reverse}"
+    newstr = []
+    newstr << "#{i.reverse}"
   end
   string = newstr.join
 end
